0ca9a99d8bae69cdb47b8aa8a2693a46c4ca7371
vijaya-66 <vijayavijaya0915@gmail.com>
2025-07-18 14:47:26 +0530

    Create TestAccess.java

---
+++ TestAccess.java
@@ -0,0 +1,16 @@
+package com.tnsif.packagesandaccessmodifiers;
+
+public class TestAccess { public static void main(Stringfl args) {
+
+AccessModifiersExample ame = new
+
+AccessModifiersExample(); ame.show();
+
+System.out.println("Public Variable: " + ame.publicVar);
+
+//System.out.println('Private Variable: + ame.privateVar);
+
+System.out.println("Protected Variable: " + ame.protectedVar);
+
+ Variable: + ame.defaultVar);
+
