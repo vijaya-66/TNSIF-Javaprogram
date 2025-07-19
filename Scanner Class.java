403cd4ae84e065c8ee72b135487cbdad9dda189d
vijaya-66 <vijayavijaya0915@gmail.com>
2025-07-19 10:08:03 +0530

    Create Scanner Class.java

---
+++ Scanner Class.java
@@ -0,0 +1,10 @@
+package com.tnsif.packagesandaccessmodifiers; import java. util. Scanner; public class ScannerClassExample { public static void main(String[] args) { Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter college name: 'I); String cname = sc.nextLine();
+
+System.out.println("Enter college code: 'I); int code = sc.nextlnt();
+
+System.out.println("The college name is: " + ename + 
+
+The college code is: " + code);
+
