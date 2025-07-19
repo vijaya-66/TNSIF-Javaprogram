73dcf8f9e10bca916680e18b2f48d1cc319d9b85
vijaya-66 <vijayavijaya0915@gmail.com>
2025-07-19 10:10:07 +0530

    Create oopsconcept.java

---
+++ oopsconcept.java
@@ -0,0 +1,14 @@
+package com.tnsif.oopsconcept;
+
+abstract class Shape{ abstract void draw();
+
+class Circle extends Shape { public void draw() {
+
+System.out.println("Drawing Circle");
+
+public class AbstractionDemo { public static void main(String[] args) {
+
+Shape s = new Circle();
+
+s. draw();
+
