e6e623d64241b1c139f89b5cd3754e23cc7826b2
vijaya-66 <vijayavijaya0915@gmail.com>
2025-07-19 10:05:42 +0530

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,28 @@
+package com.tnsif.oopsconcept;
+
+public class EncapsulationDemo {
+
+//IPL Team
+
+//Mumbai Indians Team
+
+//instance variable String name; int age; int jersyNo;
+
+* void show() { int count; } //local variable
+
+public String getName() { return name;
+
+public void setName(String name) { this.name = name; //This is refers to the current object
+
+public int getAge() { return age;
+
+public void setAge(int age) { this.age = age;
+
+public int getJersyNo() {
+
+return jersyNo;
+
+public void setJersyNo(int jersyNo) { this.jersyNo = jersyNo;
+
+//toString() Method @Override public String toString() { //Object return "EncapsulationDemo [name=' + name + age='  age + jersyNo=" + jersyNo + 
+
