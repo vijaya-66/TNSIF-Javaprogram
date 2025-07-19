21b59763f3f46893253fbceedefac85124ed1aea
vijaya-66 <vijayavijaya0915@gmail.com>
2025-07-19 10:23:05 +0530

    Create static.java

---
+++ static.java
@@ -0,0 +1,18 @@
+package com.tnsif.nonaccessmodifiers; public class StaticDemo {
+
+//static variable static int count;
+
+//static block static { count = 20;
+
+System.out.println("Shows the count value assigned as:  + count);
+
+//static method static void show() {
+
+System.out.println("Shows the static method: "+count );
+
+public static void main(Stringfl args) {
+
+System.out.println("Main method static: "+count);
+
+//calling a method show();
+
