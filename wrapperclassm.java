f8bcddcb03c71cc5b22298c21ab15bd904494628
vijaya-66 <vijayavijaya0915@gmail.com>
2025-07-19 10:13:54 +0530

    Create wrapperclassmv..java

---
+++ wrapperclassm.java
@@ -0,0 +1,18 @@
+package com.tnsif.wrapperclass; public class WrapperClassDemo { public static void main(String[] args) {
+
+//wrapper class - used to convert primitive data types to objects
+
+//and vice versa
+
+//autoboxing or boxing //unboxing
+
+int x = 5;
+
+//autoboxing
+
+Integer y = Integer.valueOf(x);
+
+//unboxing int z = y;
+
+System.out.println(y + 
+
